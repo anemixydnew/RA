adb4132623413184559dd268ea49952a9279f225
anemixydnew <wendycalizayaperez@gmail.com>
2019-09-18 23:07:15 -0400

    nombre persnalizado, solo uno... salir del programa, color al objeto

---
+++ Assets/ComparteDatos.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComparteDatos : MonoBehaviour {
+    public InputField IngresarNombre;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Nombre.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class Nombre : MonoBehaviour {
+    public InputField IngresarNombre;
+    public Text SacarNombre;
+    public void enviarAtrapar()
+    {
+        SacarNombre.text = "hola " + IngresarNombre.text + " !";
+    }
+}
